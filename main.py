70420d75d408c4d593d1846a99be970867974315
Maxwell <maxwstull@gmail.com>
2023-10-19 13:14:25 -0500

    stuck main in a function

--- main.py
+++ main.py
@@ -5,43 +5,43 @@ from classes import Hour, Forecast, FuzzyRecallDict, Recommender, CtoF
 import pandas as pd
 
 
-
-
-cur_lat = 41.946468
-cur_long = -87.647328
-
-params = {"latitude":cur_lat,
-          "longitude":cur_long,
+def getOpenMeteo(lat=41.946468, long=-87.647328):
+    params = {"latitude":lat,
+          "longitude":long,
           "timezone":"auto",
           "forecast_days":2,
           "daily":"sunrise,sunset",
           "hourly":"temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,cloudcover,windspeed_10m,uv_index,rain,snowfall,precipitation_probability"}
 
-url = "https://api.open-meteo.com/v1/forecast?"
-x = requests.get(url, params=params)
-print("API Call Returned")
-APIReturn = x.json()
-with open("test.json", "w" ) as fp:
-    json.dump(APIReturn, fp)
+    url = "https://api.open-meteo.com/v1/forecast?"
+    return requests.get(url, params=params).json()
+    
 
-forecasty = Forecast(APIReturn)
+def main():
 
 
-current_time = datetime.datetime.now()
+    APIReturn = getOpenMeteo()
+    with open("test.json", "w" ) as fp:
+        json.dump(APIReturn, fp)
 
-print(forecasty.getHour(current_time))
+    forecasty = Forecast(APIReturn)
+    current_time = datetime.datetime.now()
+    print(forecasty.getHour(current_time))
+
+    when = datetime.datetime(2023, 10, 12, 13, 0, 0)
+    if when < current_time:
+        when = current_time
+    duration = 3
+
+    print(forecasty.getRelevantHours(when, duration))
+
+    data = pd.DataFrame(forecasty.getLRList(when))
+    data['Activity'] = [1]
+    print(data)
+    rec = Recommender()
+    rec.train()
+    rec.LRPredict(data)
 
 
-when = datetime.datetime(2023, 10, 12, 13, 0, 0)
-if when < current_time:
-    when = current_time
-duration = 3
-
-print(forecasty.getRelevantHours(when, duration))
-
-data = pd.DataFrame(forecasty.getLRList(when))
-data['Activity'] = [1]
-print(data)
-rec = Recommender()
-rec.train()
-rec.LRPredict(data)
+if __name__ == "__main__":
+    main()
